0e6a79b1ef6fb4883fc7cf4f030add65f0b25f09
maxperko <maxperko@stanford.edu>
2020-05-15 13:40:44 -0700

    SRA signed reg use, test

--- lab3/hw/hdl/verilog/mips/alu.v
+++ lab3/hw/hdl/verilog/mips/alu.v
@@ -45,7 +45,7 @@ module alu (
             `ALU_XOR:   alu_result = alu_op_x ^ alu_op_y;
             `ALU_NOR:   alu_result = ~(alu_op_x | alu_op_y);
             `ALU_MUL:   alu_result = alu_op_x * alu_op_y;
-            `ALU_SRA:   alu_result = alu_op_y >>> alu_op_x[4:0];
+            `ALU_SRA:   alu_result = alu_op_y_signed >>> alu_op_x_signed[4:0];
             default:    alu_result = 32'hxxxxxxxx;   // undefined
         endcase
     end
