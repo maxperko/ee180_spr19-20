2f3011552b3ebe2205eef8e4f135d1be479b8f41
Nikhil Hira <nhira@stanford.edu>
2020-05-16 14:19:25 -0700

    inst fetch for jump_branch

--- lab3/hw/hdl/verilog/mips/instruction_fetch.v
+++ lab3/hw/hdl/verilog/mips/instruction_fetch.v
@@ -12,6 +12,7 @@ module instruction_fetch (
     input [31:0] jr_pc_id,
     input jump_reg,
     input jump_target,
+    input jump_branch,
     input [31:0] pc_id,
     input [25:0] instr_id,  // Lower 26 bits of the instruction
 
@@ -20,8 +21,9 @@ module instruction_fetch (
 
     wire [31:0] pc_id_p4 = pc_id + 3'h4;
     wire [31:0] j_addr = {pc_id_p4[31:28], instr_id[25:0], 2'b0};
+    wire [31:0] j_branch = pc_id_p4 + (instr_id[15:0] << 2);
 
-    wire [31:0] pc_next = (jump_target) ? j_addr : ((jump_reg) ? jr_pc_id : (pc + 3'h4));
+    wire [31:0] pc_next = (jump_target) ? j_addr : ((jump_reg) ? jr_pc_id : ((jump_branch) ? j_branch : (pc + 3'h4)));
 
     dffare #(32) pc_reg (.clk(clk), .r(rst), .en(en), .d(pc_next), .q(pc));
 
